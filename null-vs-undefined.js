235cc103039803a7acbce88bc319b650dbe279d9
Mehedi <mhmuffasir@gmail.com>
2021-02-04 11:57:45 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+let moyna;
+console.log(moyna);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(10, 20);
+console.log(result);
+
+const premik = {name:"smart boy", phone:03843};
+console.log(premik.address);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [3, 6, 10];
+console.log(ages[12]);
