2a4de141e1b4c00ea04ec6147f4a7a982139c0dd
Mehedi <mhmuffasir@gmail.com>
2021-02-04 19:09:29 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,11 @@
+ function sum(first, second){
+     let result = first + second;
+     if(result > 9){
+         const mood = "happy";
+         console.log(mood);
+     }
+     return result;
+ }
+
+ const output = sum(3, 7);
+ console.log(output);
