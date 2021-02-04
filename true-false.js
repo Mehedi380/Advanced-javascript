060da7fb148b8bb81be46e1c2f995b2fe95edafb
Mehedi <mhmuffasir@gmail.com>
2021-02-04 11:20:11 +0600

    true va false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//Falsy
+//0
+//""
+//undefined
+//NaN
+//null
+
+//Truthy
+//'0', " ", ' ', [], {},
+
+//const age = -4;
+let name = 10;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
